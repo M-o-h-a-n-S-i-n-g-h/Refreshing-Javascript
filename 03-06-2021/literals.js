aecb757a13b45fe67e0bb44a897b251367d738e4
mohan.singh1385664 <mohan.singh1385664@gmail.com>
2021-06-03 15:47:19 +0530

    Added Template literals

---
+++ 03-06-2021/literals.js
@@ -0,0 +1,19 @@
+const name = "Mohan Singh";
+const founder = "Microsoft";
+const age = 20;
+
+const greeting = `Hi Mr.${name}, your age is ${age}`;
+
+const email = `Hi Mr.${name},
+
+Welcome to Visual Studio Code,
+Happy Coding.
+
+Thanks and Regards,
+${founder}`;
+
+console.log(email);
+
+// Template Literals are dynamic string literals where we can print and store dynamic strings by inserting variables and values inside a tilde
+
+// We can aslo format the strings as per our customisation using template literals
