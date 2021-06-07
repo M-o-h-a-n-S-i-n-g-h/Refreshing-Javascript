4b66260eb9f908bff5809de0a072868b2c129121
mohan.singh1385664 <mohan.singh1385664@gmail.com>
2021-06-07 20:18:59 +0530

    Formatted

--- React/practicing-react/src/components/lifeCycleMethods/ComponentDidMount.jsx
+++ React/practicing-react/src/components/lifeCycleMethods/ComponentDidMount.jsx
@@ -1,37 +1,32 @@
 import React from "react";
 
 class DidMount extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         name: "No name"
-      }
-      this.getName = this.getName.bind(this);
-   }
-   
-   getName() {
-      return "Mohan Singh";
-   }
-   
-   componentDidMount() {
-      this.setState({
-         name: this.getName()
-      })
-   }
-   
-   render() {
-      return (
-        <div className="App">
-           <h1>
-              I'm From {this.props.title}
-           </h1>
-           <h2>
-              I'm {this.state.name} I loaded from "ComponentDidMount" Method
-           </h2>
-        </div>
-      
-      )
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "No name",
+    };
+    this.getName = this.getName.bind(this);
+  }
+
+  getName() {
+    return "Mohan Singh";
+  }
+
+  componentDidMount() {
+    this.setState({
+      name: this.getName(),
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>I'm From {this.props.title}</h1>
+        <h2>I'm {this.state.name} I loaded from "ComponentDidMount" Method</h2>
+      </div>
+    );
+  }
 }
 
 export default DidMount;
--- React/practicing-react/src/components/lifeCycleMethods/ComponentDidUpdate.jsx
+++ React/practicing-react/src/components/lifeCycleMethods/ComponentDidUpdate.jsx
@@ -1,43 +1,45 @@
 import React from "react";
 
 class DidUpdate extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         name: "Mohan"
-      }
-      
-      this.getName = this.getName.bind(this);
-      this.setName = this.setName.bind(this);
-   }
-   
-   getName() {
-      return this.state.name;
-   }
-   
-   setName(name) {
-      this.setState({
-         name: name
-      });
-   }
-   
-   componentDidMount() {
-      this.getName();
-      this.setName(this.state.name);
-   }
-   
-   componentDidUpdate(prevProps) {
-      if (this.state.name !== prevProps.name) {
-         this.setName(this.props.name);
-      }
-   }
-   
-   render() {
-      return <div className="App">
-         <h1>{this.props.title}</h1>
-         <h2>{this.state.name}</h2>
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Mohan",
+    };
+
+    this.getName = this.getName.bind(this);
+    this.setName = this.setName.bind(this);
+  }
+
+  getName() {
+    return this.state.name;
+  }
+
+  setName(name) {
+    this.setState({
+      name: name,
+    });
+  }
+
+  componentDidMount() {
+    this.getName();
+    this.setName(this.state.name);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.state.name !== prevProps.name) {
+      this.setName(this.props.name);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.title}</h1>
+        <h2>{this.state.name}</h2>
       </div>
-   }
+    );
+  }
 }
 
 export default DidUpdate;
